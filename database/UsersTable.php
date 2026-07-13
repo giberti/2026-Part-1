803ff8872bd381d501f8a8bd520e895396b7300d
Erik Giberti <erik@af-design.com>
2026-07-12 19:31:31 -0500

    Create UsersTable.php
    Example UsersTable Class

---
+++ database/UsersTable.php
@@ -0,0 +1,13 @@
+<?php
+
+class UsersTable {
+    public function getUserByUsernamePassword($username, $passwored) {
+      // SQL
+      // verify hash
+      return $user;
+    }
+    public function getUserByUserID($userId) {
+        // SQL
+      return $user;
+    }
+}
